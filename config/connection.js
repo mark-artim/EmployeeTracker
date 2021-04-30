87dd5609348d02ed105a7ad1897af7a7b656e3fe
Mark Artim <mark.artim@gmail.com>
2021-04-30 10:23:07 -0500

    Although it is not called, connection.js contained actual values for root password so I removed the values.

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = async function main() {
     // get the client
     const mysql = require('mysql2/promise');
     // create the connection
-    return await mysql.createConnection({host:'localhost', user: 'root', database: 'emptracker_db', password: 'Gramshammer856!'})
+    return await mysql.createConnection({host:'localhost', user: '', database: 'emptracker_db', password: ''})
     // query database
      }
 
